40bd1aa4b977d395096c8b67c2f1b16183b7269d
Hadassa Moraes <had.moraes@gmail.com>
2022-02-08 16:14:26 -0300

    create validaEmail.js

---
+++ assets/models/validaEmail.js
@@ -0,0 +1,23 @@
+function ValidateEmail(email) {
+
+    const emailValido =  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    try {
+        if (email.match(emailValido)) {
+  
+            alert("Email validado com sucesso");
+
+            return email
+        
+          } else {
+        
+            throw new Error('Email inválido')
+        
+          }
+    } catch (error) {
+        console.error(error.message)
+    }
+  
+
+  
+  }
