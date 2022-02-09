22ab895dcd2b9797ddb526aa57de808b3baccc0f
Hadassa Moraes <had.moraes@gmail.com>
2022-02-09 16:57:07 -0300

    create class GerenciadorDeEmail no validaEmail.js

--- assets/models/validaEmail.js
+++ assets/models/validaEmail.js
@@ -1,23 +1,27 @@
-function validaEmail(email) {
+class GerenciadorDeEmail {
 
-    const emailValido =  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  validaEmail(email) {
+
+    const emailValido = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     try {
-        if (email.match(emailValido)) {
-  
-            alert("Email validado com sucesso");
+      if (email.match(emailValido)) {
 
-            return email
-        
-          } else {
-        
-            throw new Error('Email inválido')
-        
-          }
+        console.log("Email validado com sucesso");
+
+        return email
+
+      } else {
+
+        throw new Error('Email inválido')
+
+      }
     } catch (error) {
-        console.error(error.message)
+      console.error(error.message)
     }
-  
 
-  
+
+
   }
+}
+
