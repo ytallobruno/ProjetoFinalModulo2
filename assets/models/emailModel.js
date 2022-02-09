7b766d32bf364da330247fa10bdf0b0d9d5ee280
Hadassa Moraes <had.moraes@gmail.com>
2022-02-09 16:59:55 -0300

    rename validaEmail.js to emailModel.js and creat confirmaEmail on class GerenciadorDeEmail

--- assets/models/emailModel.js
+++ assets/models/emailModel.js
@@ -1,5 +1,11 @@
 class GerenciadorDeEmail {
 
+  confirmaEmail(email,emailrepeat){
+    if(email === emailrepeat){
+      return true
+    }
+  }
+
   validaEmail(email) {
 
     const emailValido = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
