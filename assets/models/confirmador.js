e5b369894942197acc53b1e9c0a31360b0d31fa6
Hadassa Moraes <had.moraes@gmail.com>
2022-02-09 22:44:16 -0300

    update confirmador.js : add else in confirmaItem method

--- assets/models/confirmador.js
+++ assets/models/confirmador.js
@@ -1,12 +1,11 @@
 class Confirmador{
-    constructor(item,itemReapeat){
-        this.item = item;
-        this.itemReapeat = itemReapeat;
-    }
 
-    confirmaItem(){
-        if(this.item === this.itemReapeat){
+    confirmaItem(item,itemReapeat){
+        if(item == itemReapeat){
             return true
+          } else {
+              return false
           }
     }
 }
+
