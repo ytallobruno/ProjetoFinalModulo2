30ad0be3ffc05f9b54a934056f732882bf25c6cb
Hadassa Moraes <had.moraes@gmail.com>
2022-02-12 13:22:04 -0300

    create emailController.js, update emailModel.js and add both to cadastro.html

--- assets/models/js/emailModel.js
+++ assets/models/js/emailModel.js
@@ -1,26 +1,13 @@
 class GerenciadorDeEmail{
-
-  validaEmail(email) {
-
-    const emailValido = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-    try {
-      if (email.match(emailValido)) {
-
-        console.log("Email validado com sucesso");
-        return email
-
-      } else {
-
-        throw new Error('Email inválido')
-
+    
+  validaEmail(email){
+      const validacao =  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (email.match(validacao)){
+          this.email = email;
+          return true;
+      } else{
+          return false;
       }
-    } catch (error) {
-      console.error(error.message)
-    }
-
-
-
   }
 }
 
